7ccfd9cd3f2284eb56362a11e179177d8d4a6d22
unknown <anjalidhingra1808@gmail.com>
2020-07-09 22:13:43 +0530

    first commit

---
+++ NumberPuzzleQuestion/src/CustomNumberPuzzleControl.java
@@ -0,0 +1,77 @@
+import java.awt.*;
+
+class CustomNumberPuzzleControl extends NumberPuzzleControl {
+	public int getWidth() {
+		return 200;
+	}
+	public int getHeight() {
+		return 250;
+	}
+	public int getXPosition() {
+		return 200;
+	}
+	public int getYPosition() {
+		return 200;
+	}
+	public String getTitle(){
+		return "Number Puzzle";
+	}
+	public int getShuffleButtonFontSize() {
+		return 12;
+	}
+	public int getNumbersFontSize() {
+		return 12;
+	}
+	public Color getEmptyButtonColor() {
+		return Color.WHITE;
+	}
+	public String getWinnerMessage() {
+		return "Congrats, you have won!";
+	}
+
+	// The following three methods have to be written by the participants...
+
+	public int handleButtonClicked(NumberPuzzleGame game){
+		int emptyCellId = game.getEmptyCellId();
+		Button buttonClicked = game.getButtonClicked();
+		Button[] buttons = game.getButtons();
+		
+		//Your logic here		
+		
+		return emptyCellId;
+
+	}
+	public int[] getRandomNumbersForGrid() {
+		int arr[] = new int[15];
+		
+		//Your logic here
+		int a = getRandomNumber();
+		 while (n > 1) {
+    	int r = RANDOM.nextInt(n--);
+    	int tmp = arr[r];
+    	arr[r] = arr[n];
+    	arr[n] = arr;
+  }
+		
+		
+		return arr;
+	}
+	public boolean checkForWinner(Button[] buttons)
+	{
+	
+		
+		// Your Logic here
+		int arr[] = getIntegerArrayOfButtonIds(buttons);
+		
+
+		int n = arr.length;
+
+  		for (int i = 0; i < n; i++) {
+    		if (arr[i] != i + 1)
+     			return false;			
+  		}
+
+  		return true;
+
+	}
+}
